ae1d433bd32d436b2749d290b935663bc565ac0a
tovne <h23tovhe@du.se>
2025-11-11 18:06:57 +0100

    uppgift 4

--- script.js
+++ script.js
@@ -18,8 +18,10 @@ try{
 try{
     console.log("Utanför blocket:", y)
 } catch(e){
-    console.log("y är inte definierad utanför blocket(let)")
+    console.log("y är inte definierad utanför blocket(const)")
 }
+
+console.log("y finns kvar utanförblocket(var):")
 
 /*
 Reflektion uppgift 2:
@@ -31,6 +33,19 @@ Reflektion uppgift 2:
 //
 //Uppgift 3
 //
+
+console.log("'3' == 3:", '3'== 3);      //True
+console.log("'3' === 3:", '3'=== 3);    //false
+
+console.log("'NaN' === NaN:", 'NaN'=== NaN);    //false
+
+console.log("'null' == undefined:", 'null'== undefined);    //true
+console.log("'null' === undefined:", 'null'=== undefined);  //false
+
+//Ternary operatior:
+let check = undefined ? "truthy": "falsy";
+console.log("undefined är:", check);
+
 
 
 /*
@@ -47,7 +62,31 @@ Reflektion uppgift 3:
 //Uppgift 4
 //
 
+function greet(name){
+    return "Hej" + name;
+}
 
+console.log(greet("Tove"));
+
+//variabel i global scope
+let name = "Lisa";
+console.log("Global variabel name:", name);
+
+
+//arrow-function
+const greetArrow = (name) => "Hej" + name;
+console.log(greetArrow("Mikaela"));
+
+
+//nytt block
+{
+    let name = "Erik";
+    console.log("Inuti blocket:", name)
+    console.log(greet(name));
+
+}
+
+console.log("Globalt name igen: ", name);
 
 /*
 Reflektion uppgift 4:
