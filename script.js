90c5c2fa8c05b6533de179ac4186c173d31a74fd
tovne <h23tovhe@du.se>
2025-11-11 18:00:25 +0100

    uppgift 3

---
+++ script.js
@@ -0,0 +1,56 @@
+//
+//Uppgift 2
+//
+
+{
+    let x = 10;
+    const y = 20;
+    var z = 30;
+    console.log("Inuti blocket:", x, y, z);
+}
+
+try{
+    console.log("Utanför blocket:", x)
+} catch(e){
+    console.log("x är inte definierad utanför blocket(let)")
+}
+
+try{
+    console.log("Utanför blocket:", y)
+} catch(e){
+    console.log("y är inte definierad utanför blocket(let)")
+}
+
+/*
+Reflektion uppgift 2:
+
+
+*/
+
+
+//
+//Uppgift 3
+//
+
+
+/*
+Reflektion uppgift 3:
+
+
+*/
+
+
+
+
+
+//
+//Uppgift 4
+//
+
+
+
+/*
+Reflektion uppgift 4:
+
+
+*/
